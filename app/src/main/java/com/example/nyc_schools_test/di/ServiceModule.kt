16cc4dcd31aa49ef1ca02ca6a56267fa577f9244
DanielCaballero <danielcabaci95@gmail.com>
2022-06-16 16:30:50 -0400

    Commponent of service module di modified

--- app/src/main/java/com/example/nyc_schools_test/di/ServiceModule.kt
+++ app/src/main/java/com/example/nyc_schools_test/di/ServiceModule.kt
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 class ServiceModule {
     @Singleton
     @Provides
